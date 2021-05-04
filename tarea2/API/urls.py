744639e33fad1598c13617a1a0fe7b4c118300ba
pablo molina <pmolina1@uc.cl>
2021-05-03 22:05:04 -0400

    septimo push

--- tarea2/API/urls.py
+++ tarea2/API/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('artists/<str:pk>/', views.ArtistDetail.as_view()),
     path('artists/<str:pk>/albums', views.AlbumList.as_view()),
     path('artists/<str:pk>/tracks', views.ArtistTrackList.as_view()), 
-    path('albums/',views.AlbumListAll.as_view()),
+    path('albums',views.AlbumListAll.as_view()),
     path('albums/<str:pk>/', views.AlbumDetail.as_view()),
 	path('albums/<str:pk>/tracks', views.TrackList.as_view()), 
 	path('tracks/',views.TrackListAll.as_view()),				
